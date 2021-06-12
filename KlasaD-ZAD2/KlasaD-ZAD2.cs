56cbd8e1c9ce0d2e5299c5534e91dd99f7529456
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2021-06-12 15:05:11 +0200

    Add files via upload

---
+++ KlasaD-ZAD2/KlasaD-ZAD2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasaD_ZAD2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Unesena vrijednost:");
+
+
+
+            int vrijednost = 100;
+
+
+
+            bool b = true;
+            KlasaD SuprotniPrvi = new KlasaD();
+
+            KlasaD SuprotniDrugi = new KlasaD();
+
+            Console.WriteLine("bool " + SuprotniPrvi.SuprotnoPrvi(b) + ", broj " + SuprotniPrvi.SuprotnoDrugi(vrijednost));
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ KlasaD-ZAD2/KlasaD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasaD_ZAD2
+{
+    public class KlasaD
+    {
+
+
+        public bool SuprotnoPrvi(bool a)
+        {
+            return a != a;
+            
+        }
+        public int  SuprotnoDrugi(int a)
+        {
+            return a*- 1;
+        }
+
+    }
+}
